0ade8ff86d6bee088da54e797e55cf2d08f990aa
Shrinidhi Sridhar <shrinidhisridhar30@gmail.com>
2018-05-24 14:18:45 -0400

    Fix indentation issues

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ var $ball = $('.ball');
 
 $('#btn-right').on('click', function () {
   var newLeft = $ball.offset().left + 10;
+
   $ball.css('left', newLeft);
 });
 
 $('#btn-left').on('click', function () {
   var newRight = $ball.offset().left - 10;
+
   $ball.css('left', newRight);
 });
 
-$('#btn-up').on('click', function() {
-  var newUp = $ball.offset().top -10;
+$('#btn-up').on('click', function () {
+  var newUp = $ball.offset().top - 10;
+
   $ball.css('top', newUp);
 });
 
-$('#btn-down').on('click', function() {
+$('#btn-down').on('click', function () {
   var newDown = $ball.offset().top + 10;
+
   $ball.css('top', newDown);
-})
+});
